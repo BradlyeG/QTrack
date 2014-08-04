3f114e55d640b8775a8116521bde8484fcfc790c
Bradlye Gordon <gordonbradlye@gmail.com>
2014-08-04 18:20:50 -0400

    Worked on Main UI, still needs camera feed

--- src/gordon/qMainUI.java
+++ src/gordon/qMainUI.java
@@ -31,6 +31,18 @@ public class qMainUI extends javax.swing.JFrame {
         qQRCodeWindow = new javax.swing.JPanel();
         serialLabel = new javax.swing.JLabel();
         assetLabel = new javax.swing.JLabel();
+        serialTextField = new javax.swing.JTextField();
+        assetTextField = new javax.swing.JTextField();
+        dateLabel = new javax.swing.JLabel();
+        dateTextField = new javax.swing.JTextField();
+        dateOutLabel = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        prevIssueScrollPane = new javax.swing.JScrollPane();
+        prevIssueTextArea = new javax.swing.JTextArea();
+        currentIssueScrollPane = new javax.swing.JScrollPane();
+        currentIssueTextArea = new javax.swing.JTextArea();
+        prevIssueLabel = new javax.swing.JLabel();
+        currIssueLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,18 +52,48 @@ public class qMainUI extends javax.swing.JFrame {
         qQRCodeWindow.setLayout(qQRCodeWindowLayout);
         qQRCodeWindowLayout.setHorizontalGroup(
             qQRCodeWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 134, Short.MAX_VALUE)
+            .addGap(0, 166, Short.MAX_VALUE)
         );
         qQRCodeWindowLayout.setVerticalGroup(
             qQRCodeWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 134, Short.MAX_VALUE)
+            .addGap(0, 166, Short.MAX_VALUE)
         );
 
         serialLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
         serialLabel.setText("Device Name:");
 
         assetLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
-        assetLabel.setText("jLabel1");
+        assetLabel.setText("Asset Tag:");
+
+        serialTextField.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+
+        assetTextField.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+
+        dateLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        dateLabel.setText("Date In:");
+
+        dateTextField.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+
+        dateOutLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        dateOutLabel.setText("Date Out:");
+
+        jTextField1.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+
+        prevIssueTextArea.setColumns(20);
+        prevIssueTextArea.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        prevIssueTextArea.setRows(5);
+        prevIssueScrollPane.setViewportView(prevIssueTextArea);
+
+        currentIssueTextArea.setColumns(20);
+        currentIssueTextArea.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        currentIssueTextArea.setRows(5);
+        currentIssueScrollPane.setViewportView(currentIssueTextArea);
+
+        prevIssueLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        prevIssueLabel.setText("Previous Issue");
+
+        currIssueLabel.setFont(new java.awt.Font("Umpush", 0, 15)); // NOI18N
+        currIssueLabel.setText("Current Issue");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -59,24 +101,79 @@ public class qMainUI extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(qQRCodeWindow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(qQRCodeWindow, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(dateLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(dateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(dateOutLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(serialLabel)
-                    .addComponent(assetLabel))
-                .addContainerGap(384, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(105, 105, 105)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(serialTextField)
+                                    .addComponent(currentIssueScrollPane)
+                                    .addComponent(prevIssueScrollPane)
+                                    .addComponent(assetTextField)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(178, 178, 178)
+                                .addComponent(serialLabel)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(prevIssueLabel, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(currIssueLabel, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(191, 191, 191)
+                        .addComponent(assetLabel)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(qQRCodeWindow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(serialLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(serialTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(assetLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(assetTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(assetLabel))
-                    .addComponent(qQRCodeWindow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(334, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(dateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(dateLabel))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(dateOutLabel)
+                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(prevIssueScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addComponent(prevIssueLabel)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(currentIssueScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(currIssueLabel)
+                        .addGap(83, 83, 83))))
         );
 
         pack();
@@ -119,7 +216,19 @@ public class qMainUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel assetLabel;
+    private javax.swing.JTextField assetTextField;
+    private javax.swing.JLabel currIssueLabel;
+    private javax.swing.JScrollPane currentIssueScrollPane;
+    private javax.swing.JTextArea currentIssueTextArea;
+    private javax.swing.JLabel dateLabel;
+    private javax.swing.JLabel dateOutLabel;
+    private javax.swing.JTextField dateTextField;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel prevIssueLabel;
+    private javax.swing.JScrollPane prevIssueScrollPane;
+    private javax.swing.JTextArea prevIssueTextArea;
     private javax.swing.JPanel qQRCodeWindow;
     private javax.swing.JLabel serialLabel;
+    private javax.swing.JTextField serialTextField;
     // End of variables declaration//GEN-END:variables
 }
